afa108a807f8b7bcaa5c677b5d3ae616ab55add6
masiljangajji <xmfpdlsj0508@gmail.com>
2024-09-18 00:40:01 +0900

    code deploy test

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
-ROOT_PATH="/home/ec2-user/spring-github-action"
+ROOT_PATH="/home/ubuntu/spring-github-action"
 JAR="$ROOT_PATH/application.jar"
 STOP_LOG="$ROOT_PATH/stop.log"
-SERVICE_PID=$(pgrep -f $JAR) # 실행 중인 Spring 서버의 PID
+SERVICE_PID=$(pgrep -f $JAR) # 실행중인 Spring 서버의 PID
 
 if [ -z "$SERVICE_PID" ]; then
-  echo "서비스가 실행 중이지 않음" >> $STOP_LOG
+  echo "서비스 NouFound" >> $STOP_LOG
 else
-  echo "서비스 종료 중..." >> $STOP_LOG
+  echo "서비스 종료 " >> $STOP_LOG
   kill "$SERVICE_PID"
-  # kill -9 $SERVICE_PID # 강제 종료를 원하면 이 명령어 사용
-  echo "서비스 종료 완료" >> $STOP_LOG
+  # kill -9 $SERVICE_PID # 강제 종료를 하고 싶다면 이 명령어 사용
 fi
